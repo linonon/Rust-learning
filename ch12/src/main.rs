b26ff10ece4442e00fbb315486f5fa7e1adb01a1
linonon-WSL2 <linonon@WSL2.com>
2022-07-25 16:43:10 +0800

    Update: ch12 finshed

--- ch12/src/main.rs
+++ ch12/src/main.rs
@@ -1,6 +1,17 @@
-use std::env;
+use std::{env, process};
+
+use ch12::Config;
 fn main() {
-    println!("Hello, world!");
     // cargo run substring xxx.txt
     let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = ch12::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
 }
